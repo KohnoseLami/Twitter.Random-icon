04b7c9c0644d00f5bfec41b7b8f2c7f14db39396
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2020-08-19 23:39:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import tweepy
 import time
 import glob
 
-consumer_key = "54Xe634pzhdYJs3IDT9HnnJj4"
-consumer_secret = "5Q6NIBRg0ZLafOAazEKl5vu5xhURRWnn6RxuYAdJIkqayN1YbR"
-access_key = "1094575017679962112-h62W0c2XHQ6cBm44zKZiVwE7kqhpAG"
-access_secret = "tFq5I9UjuUMqbVv0ZvI3c6UnyxER2wqkAtAu4ZP01H43f"
- 
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
@@ -16,7 +16,7 @@ def change_icon():
 
     while True:
 
-        icons = [r.split('/')[-1] for r in glob.glob('*.jpg')]
+        icons = [r.split("/")[-1] for r in glob.glob("*.jpg")]
         icon = random.choice(icons)
         api.update_profile_image(icon)
         time.sleep(600)
